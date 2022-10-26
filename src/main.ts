e10db3d80f05ba5f2231ed37d6f7b64a27ab236b
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-26 00:30:03 -0300

    :recycle: implement validation pipe for controllers

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './shared/services/prisma/prisma.service';
@@ -5,7 +6,10 @@ import { PrismaService } from './shared/services/prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { config } = app.get(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.port);
+
   const prismaServiceInstance = app.get(PrismaService);
   await prismaServiceInstance.enableShutdownHooks(app);
 }
