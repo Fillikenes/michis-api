182c55706c10a596c0a677a4947385174dbb145b
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-29 04:05:36 -0300

    :test_tube: added unit test for prisma service

--- src/shared/services/prisma/prisma.service.spec.ts
+++ src/shared/services/prisma/prisma.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
 
+jest.mock('@prisma/client');
 describe('PrismaService', () => {
   let service: PrismaService;
 
@@ -12,7 +13,37 @@ describe('PrismaService', () => {
     service = module.get<PrismaService>(PrismaService);
   });
 
+  afterEach(() => jest.clearAllMocks());
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('#onModuleInit', () => {
+    const connectSpy = jest.fn();
+
+    beforeEach(() => {
+      service.$connect = connectSpy;
+    });
+
+    it('should execute connect method to stablish connection with db', async () => {
+      await service.onModuleInit();
+      expect(connectSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('#enableShutdownHooks', () => {
+    beforeEach(() => {
+      service.$on = jest.fn().mockImplementation((value: string, cb) => {
+        expect(value).toEqual('beforeExit');
+        cb();
+      });
+    });
+
+    it('should force the close of nest app when before exit signal is emitted', async () => {
+      const app = { close: jest.fn() } as any;
+      await service.enableShutdownHooks(app);
+      expect(app.close).toHaveBeenCalled();
+    });
   });
 });
