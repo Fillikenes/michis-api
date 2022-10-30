b34ceb58f005d81a30859425b2bfd5ecdcef8d9b
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-29 23:29:14 -0300

    :test_tube: added unit test for service of consume frequency

--- src/modules/consume-frequency/consume-frequency.service.ts
+++ src/modules/consume-frequency/consume-frequency.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { ConsumeFrequency } from '@prisma/client';
+import { PrismaService } from '../../shared/services/prisma/prisma.service';
 
 @Injectable()
-export class ConsumeFrequencyService {}
+export class ConsumeFrequencyService {
+  private readonly model = this.prismaService.consumeFrequency;
+
+  constructor(private readonly prismaService: PrismaService) {}
+
+  public async getConsumeFrequencies(): Promise<ConsumeFrequency[]> {
+    return this.model.findMany();
+  }
+
+  public async getConsumeFrequencyById(id: string): Promise<ConsumeFrequency> {
+    return this.model.findFirst({ where: { id } });
+  }
+
+  public async createConsumeFrequency(
+    frequency: string,
+  ): Promise<ConsumeFrequency> {
+    return this.model.create({
+      data: {
+        frequency,
+      },
+    });
+  }
+
+  public async updateConsumeFrequency(
+    id: string,
+    frequency: string,
+  ): Promise<ConsumeFrequency> {
+    return this.model.update({
+      where: { id },
+      data: { frequency },
+    });
+  }
+
+  public async deleteConsumeFrequencyById(
+    id: string,
+  ): Promise<ConsumeFrequency> {
+    return this.model.delete({
+      where: { id },
+    });
+  }
+}
